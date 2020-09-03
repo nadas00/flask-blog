7a3e80de9a91675e28380d76b365409ac4d64786
hasan çiftçi <se.hasancifti@gmail.com>
2020-09-03 22:57:55 +0300

    Registration implementation.

--- app/models.py
+++ app/models.py
@@ -13,8 +13,6 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password_hash,password)
-
-
     def __repr__(self):
         return '<User {}>'.format(self.username)   
 
